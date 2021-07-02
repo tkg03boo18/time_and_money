5054d844364f759db433359b8fb352e57ee4e188
tkg03boo18 <tkg03boo18@gmail.com>
2021-07-02 15:38:25 +0000

    [Update]タスク管理ページ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,32 +1,46 @@
 class TasksController < ApplicationController
-  
+
   def index
     @user = current_user
     @tasks = @user.tasks
   end
-  
+
   def new
     @task = Task.new
   end
-  
+
   def create
-    @task = Task.new
+    @task = Task.new(task_params)
     @task.save
     redirect_to tasks_path
   end
-  
+
   def show
+    @task = Task.find(params[:id])
   end
-  
+
   def edit
+    @task = Task.find(params[:id])
   end
-  
+
   def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
   end
-  
+
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
-  
-  
-  
+
+  private
+
+  def task_params
+    params.require(:task).permit(:user_id, :title, :body, :importance, :genre, :deadline, :fin)
+  end
+
+
+
 end
