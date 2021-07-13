97f4260faf2aa65e6fe5f5b5cc0c60ad398723af
tkg03boo18 <tkg03boo18@gmail.com>
2021-07-13 16:55:21 +0000

    [Update]予定管理画面

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -44,12 +44,12 @@ class PlansController < ApplicationController
   def update
     respond_to do |format|
       if @plan.update(plan_params)
-        format.html { redirect_to @plan }
-        format.json { render :show, status: :ok, location: @plan }
+        # format.html { redirect_to plans_path }
+        # format.json { render :show, status: :ok, location: @plan }
         format.js { @status = "success" }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @plan.errors, status: :unprocessable_entity }
+        # format.html { render :edit, status: :unprocessable_entity }
+        # format.json { render json: @plan.errors, status: :unprocessable_entity }
         format.js { @status = "fail" }
       end
     end
